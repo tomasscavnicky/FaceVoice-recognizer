ea493ab97a59d56d62e007a1074848d506ada158
Radim Lipka <xlipka01@stud.fit.vutbr.cz>
2016-04-30 20:36:28 +0200

    Added silence remover from every sound record - sound_library.py

--- SPEAKER_VERIF/train.py
+++ SPEAKER_VERIF/train.py
@@ -13,65 +13,22 @@ __email__ = "xlipka01@stud.fit.vutbr.cz, xscavn00@stud.fit.vutbr.cz"
 __date__ = "29.4.2016"
 
 import re, getopt, wave, sys, os, re, struct, numpy as np
+
 from features import mfcc
 from features import logfbank
 
+from sound_library import wav_to_mfcc
+
+
 import neurolab as nl
 
-#import neurolab as nl
+sampling = int(16000)
+channels = int(1)
 
-sampling = 16000
-
-def short_to_mfcc(signal):
-
-	global sampling
-
-	mfcc_features = mfcc(signal, samplerate=sampling, winlen=0.025, winstep=0.01, numcep=13, nfilt=26, nfft=512, lowfreq=0, highfreq=1000, preemph=0.97, ceplifter=22, appendEnergy=True)
-	fbank_features = logfbank(signal, sampling)
-
-	#print(fbank_features[1:3,:])
-
-	#return fbank_features[1:3,:]
-	return fbank_features[1:2,:]
-
-def wav_to_short(wave_file):
-
-	global sampling
-
-	# Library wave 
-	w = wave.open(wave_file)
-
-	# Starting from 1. sec because of booms
-	if(sampling >= w.getnframes()):
-		return
-
-	length = w.getnframes() - sampling
-
-	w.setpos(sampling)
-	wav_samples = w.readframes(length)
-
-	# Library struct 
-	short_samples = struct.unpack("%ih" % (length* w.getnchannels()), wav_samples)
-	return np.asarray(short_samples)
-
-def wav_to_mfcc(path, w_files, mfcc_output):
-
-	files = list()
-
-	for w_file in sorted(w_files):
-
-		if not (w_file.endswith(".wav")):
-			continue
-
-		print("Processing: " + w_file)
-		content = wav_to_short(path + w_file)
-		files.append(path + w_file)
-
-		mfcc_output.append(short_to_mfcc(content).tolist()[0])
-
-	return files
 
 def main(args, argv):
+
+# PARSING ARGUMENTS
 
 	train_input_data=list()
 	test_input_data=list()
@@ -88,6 +45,8 @@ def main(args, argv):
 	except:
 		exit(2)
 
+# LOADING TRAINING DATA
+
 	print("LOADING TRAIN DATA")
 
 	w_files_train = wav_to_mfcc(argv[0], w_files_train, train_input_data)
@@ -97,6 +56,8 @@ def main(args, argv):
 	w_files_dev = wav_to_mfcc(argv[1], w_files_dev, train_input_data)
 
 	train_output_data += [[0.0]] * (len(train_input_data) - len(train_output_data))
+
+# TRAINING NEURAL NETWORK
 
 	print("\nTRAINING\n")
 
@@ -113,10 +74,12 @@ def main(args, argv):
 
 	print("SHAPE: " + str(len(train_input_data[0])))
 
-	net = nl.net.newff([[minimal, maximal]] * len(train_input_data[0]), [26, 26, 26, 1])
+	net = nl.net.newff([[minimal, maximal]] * len(train_input_data[0]), [26, 26, 1])
 
 	# Train process
 	er = net.train(np.asarray(train_input_data), np.asarray(train_output_data), show=1, goal=0.2)
+
+# LOADING TESTING DATA
 
 	print("LOADING TEST DATA")
 
@@ -127,6 +90,8 @@ def main(args, argv):
 	w_non_files_dev = wav_to_mfcc(argv[3], w_non_files_dev, test_input_data)
 
 	test_output_data += [[0.0]] * (len(test_input_data) - len(test_output_data))
+
+# TESTING
 
 	print("\nTESTING\n")
 
